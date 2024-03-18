f4f29faf2868c16c0359fce3aa96c7c1929001a3
blakesekelsky <6610741+blakesekelsky@users.noreply.github.com>
2024-03-18 21:54:09 +0800

    unnecessary

--- src/Board.hpp
+++ src/Board.hpp
@@ -14,13 +14,6 @@ struct Board {
     // update tiles
     for ( int i : {0, 1, 2, 3} ) {
       for ( int j : {0, 1, 2, 3} ) {
-        tiles[i][j].value = values[i][j];
-      }
-    }
-
-    // update board
-    for ( int i : {0, 1, 2, 3} ) {
-      for ( int j : {0, 1, 2, 3} ) {
         tiles[i][j] = Tile(2 + (112 * i), 2 + (112 * j), 110, values[i][j]);
       }
     }
